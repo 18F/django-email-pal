1dea3f3ad69ada851e20da671f022115f4b60ff5
Atul Varma <varmaa@gmail.com>
2017-05-25 11:16:24 -0700

    Add SendableEmail class.

--- emailpal/tests/test_stuff.py
+++ emailpal/tests/test_stuff.py
@@ -1,4 +1,34 @@
-from .. import VERSION
+from mypy_extensions import TypedDict
 
-def test_tests_work():
+from .. import VERSION, SendableEmail
+
+def test_version_is_a_string():
     assert type(VERSION) is str
+
+
+MyContext = TypedDict('MyContext', {'full_name': str})
+
+class MySendableEmail(SendableEmail[MyContext]):
+    template_name = 'my_sendable_email.html'
+    subject = 'howdy {full_name}!'
+
+
+def test_sending_email_works():
+    ctx = MyContext(full_name='boop jones')
+    e = MySendableEmail()
+
+    assert e.render_subject(ctx) == 'howdy boop jones!'
+
+    html = e.render_body_as_html(ctx)
+    text = e.render_body_as_plaintext(ctx)
+
+    assert "Hello boop jones" in html
+    assert "Hello boop jones" in text
+
+    assert "I am plaintext" in text
+    assert "I am not HTML" in text
+
+    assert "I am HTML" in html
+    assert "I am <strong>not</strong> plaintext" in html
+
+    e.send_messages(ctx)
